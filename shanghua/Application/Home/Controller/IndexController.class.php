2400142bb66450e4867d818c9f3c9f186a4dfb32
gao zhi qin <540099154@qq.com>
2016-12-11 16:02:00 +0800

    后台

--- shanghua/Application/Home/Controller/IndexController.class.php
+++ shanghua/Application/Home/Controller/IndexController.class.php
@@ -4,14 +4,10 @@ use Think\Controller;
 class IndexController extends Controller {
     public function index(){
 
-    	//1,获取酒店分类信息
-    	$categoryModel=M('categorys');
-    	$data=$categoryModel->select();
-    	$this->assign('category',$data);
-    	
     	$this->display();
     }
-    public function test(){
-    	$this->show('我在test方法里','utf-8');
+    public function indexe(){
+
+        $this->display();
     }
 }
---
+++ shanghua/Application/Home/Controller/JobController.class.php
@@ -0,0 +1,61 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class JobController extends Controller {
+    public function detail1(){
+
+    	$this->display();
+    }
+    public function detail1e(){
+
+        $this->display();
+    }
+    public function detail2(){
+
+        $this->display();
+    }
+    public function detail2e(){
+
+        $this->display();
+    }
+    public function detail3(){
+
+        $this->display();
+    }
+    public function detail3e(){
+
+        $this->display();
+    }
+    public function detail4(){
+
+        $this->display();
+    }
+    public function detail14e(){
+
+        $this->display();
+    }
+    public function detail5(){
+
+        $this->display();
+    }
+    public function detail15e(){
+
+        $this->display();
+    }
+    public function join1(){
+
+        $this->display();
+    }
+    public function join1e(){
+
+        $this->display();
+    }
+    public function join2(){
+
+        $this->display();
+    }
+    public function join2e(){
+
+        $this->display();
+    }
+}
