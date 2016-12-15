00a44ceac04b4ff9c1572e290419f7847caea189
liutianmeng <764913428@qq.com>
2016-12-15 09:34:22 +0800

    Admin

---
+++ shanghua/Application/Admin/Controller/ApplyenController.class.php
@@ -0,0 +1,37 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class ApplyenController extends Controller {
+	public function __construct(){
+        parent::__construct();
+        if(!isLogin()){
+            $this->error("请先登录",U("Admin/login"));
+        }
+    }
+     public function lists(){
+    	$applyenModel = D("applyen");
+		$applyen = $applyenModel->select();
+		$this->assign('applyen',$applyen);
+		$this->display();  
+    }
+     public function delete() {
+        //全部删除
+        $id = $_GET['applyenId'];
+        if(is_array($id)){
+            foreach($id as $value){
+                D("applyen")->delete($value);
+            }  
+            $this->success("批量删除成功！",U("lists"));
+        } 
+        //单个删除
+        else{
+            $applyenModel = D("applyen");
+            if($applyenModel->where("id=$id")->delete()){
+                $this->success("删除成功",U("Applyen/lists"));
+            }
+            else{
+                $this->error($applyenModel->geterror());
+            }           
+        }      
+    }
+}
---
+++ shanghua/Application/Admin/Controller/JobenController.class.php
@@ -0,0 +1,86 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class JobenController extends Controller {
+	public function __construct(){
+        parent::__construct();
+        if(!isLogin()){
+            $this->error("请先登录",U("Admin/login"));
+        }
+    }
+     public function lists(){
+    	$jobenModel = D("joben");
+		$joben = $jobenModel->select();
+		$this->assign('joben',$joben);
+		$this->display();  
+    }
+    public function get_time(){
+    	return date("Y-m-d");
+    }
+    public function add(){
+    	$time = $this->get_time();
+    	$this->assign('time',$time);
+    	$this->display();
+    }
+    public function doAdd(){
+	        if(!IS_POST){
+	            exit("bad request");
+	        }
+	        $jobenModel = D("joben");
+	        if(!$jobenModel->create()){
+	            $this->error($jobenModel->getError());          
+	        }
+	        if($jobenModel->add()){
+	            $this->success("添加成功",U("lists"));
+	        }
+	        else{
+	            $this->error("添加失败");
+	        }
+    	}
+    public function delete() {
+        //全部删除
+        $id = $_GET['jobenId'];
+        if(is_array($id)){
+            foreach($id as $value){
+                D("job")->delete($value);
+            }  
+            $this->success("批量删除成功！",U("lists"));
+        } 
+        //单个删除
+        else{
+            $jobenModel = D("joben");
+            if($jobenModel->where("id=$id")->delete()){
+                $this->success("删除成功",U("Job/lists"));
+            }
+            else{
+                $this->error($jobenModel->geterror());
+            }       
+        }      
+    }
+    public function edit() {
+
+	        $id = intval($_GET['id']);
+	        if ($id == '') {
+	            exit("error param");
+	        }
+	        $joben = M("joben")->find($id);
+	        $this->assign("joben", $joben);
+	        $time = $this->get_time();
+    		$this->assign('time',$time);
+	        $this->display();
+    	}
+
+   		 public function doEdit() {
+	        if (!IS_POST) {
+	            exit("error param");
+	        }
+	        $jobenModel = D("joben");
+	        if ($jobenModel->create() && $jobenModel->save()) {
+	            $this->success("修改成功!", U('Joben/lists'));
+	        }
+	        else {
+	            // $this->error($jobModel->getError());
+	        }
+    	}
+    
+}
