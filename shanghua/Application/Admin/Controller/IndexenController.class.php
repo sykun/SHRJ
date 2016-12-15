771b9f9901685654e94cf47f720b4c9cccc71186
14wangna <328007406@qq.com>
2016-12-15 10:08:44 +0800

    后台

---
+++ shanghua/Application/Admin/Controller/IndexenController.class.php
@@ -0,0 +1,19 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class IndexenController extends Controller {
+    public function __construct(){
+        parent::__construct();
+        if(!isLogin()){
+            $this->error("请先登录",U("Admin/login"));
+        }
+    }
+    public function index(){
+        $this->display();
+    }
+    public function loginout(){
+    	session('username',null);
+        $this->success('退出成功',U("Admin/login"));
+    }
+
+}
