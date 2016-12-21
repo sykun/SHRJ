72a1e4bb19a355b40087e18c76bf727a2291ef1e
wangyanyan123 <1436214639@qq.com>
2016-12-21 11:02:45 +0800

    啊啊啊啊啊

--- shanghua/Application/Home/Controller/SuccessController.class.php
+++ shanghua/Application/Home/Controller/SuccessController.class.php
@@ -4,19 +4,27 @@ use Think\Controller;
 class SuccessController extends Controller {
    
     public function gl(){
-
+        $caseModel=M('case');
+        $data=$caseModel->select();
+        $this->assign("case",$data);
         $this->display();
     }
     public function gle(){
-
+$caseenModel=M('caseen');
+        $data=$caseenModel->select();
+        $this->assign("caseen",$data);
         $this->display();
     }
     public function VE(){
-
+        $caseModel=M('case');
+        $data=$caseModel->select();
+        $this->assign("case",$data);
         $this->display();
     }
     public function VEe(){
-
+$caseenModel=M('caseen');
+        $data=$caseenModel->select();
+        $this->assign("caseen",$data);
         $this->display();
     }
     
