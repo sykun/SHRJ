6f94a52977d2b8f58d5217fa1758a69e45f4f99c
wangyanyan123 <1436214639@qq.com>
2016-12-26 14:24:24 +0800

    啊啊啊啊啊

--- shanghua/Application/Admin/Controller/AdminController.class.php
+++ shanghua/Application/Admin/Controller/AdminController.class.php
@@ -13,7 +13,8 @@ class AdminController extends Controller {
             $result = $adminUsersModel->where($condition)->count();
             if($result>0){  //能查到数据，说明用户名密码正确
                 session("username",I("post.username"));
-                $this->success("登录成功",U("Index/index"));
+                //$this->success("登录成功",U("Index/index"));
+                 $this->redirect('lists?p=1',0);
             }
             else{
                 $this->error("用户名或密码不正确");
