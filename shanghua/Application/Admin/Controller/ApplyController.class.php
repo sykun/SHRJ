85d9a722305924ce830b9c36292eb01d5c64591d
wangyanyan123 <1436214639@qq.com>
2016-12-26 14:45:45 +0800

    啊啊啊啊啊

--- shanghua/Application/Admin/Controller/ApplyController.class.php
+++ shanghua/Application/Admin/Controller/ApplyController.class.php
@@ -9,10 +9,20 @@ class ApplyController extends Controller {
         }
     }
      public function lists(){
-    	$applyModel = D("apply");
-		$apply = $applyModel->select();
-		$this->assign('apply',$apply);
-		$this->display();  
+    	    $applyModel = D("apply");
+            $cut=8;
+            $currentPage = I("get.p");
+            $offset = ($currentPage-1) * $cut;
+            $apply=$applyModel->where()->limit("$offset,$cut")->select();
+            $this->assign("apply",$apply);
+
+            $count = $applyModel->count();
+            $Page = new \Think\Page($count, $cut);
+            $show = $Page->show();
+            $this->assign("page", $show);
+
+            $this->display(); 
+ 
     }
      public function delete() {
         //全部删除
@@ -21,13 +31,15 @@ class ApplyController extends Controller {
             foreach($id as $value){
                 D("apply")->delete($value);
             }  
-            $this->success("批量删除成功！",U("lists"));
+            //$this->success("批量删除成功！",U("lists?p=1"));
+           $this->redirect('lists?p=1',0);
         } 
         //单个删除
         else{
             $applyModel = D("apply");
             if($applyModel->where("id=$id")->delete()){
-                $this->success("删除成功",U("Apply/lists"));
+               // $this->success("删除成功",U("Apply/lists?p=1"));
+                $this->redirect('lists?p=1',0);
             }
             else{
                 $this->error($applyModel->geterror());
