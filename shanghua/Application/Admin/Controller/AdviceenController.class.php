fedb2799521109c29d9278887cba9d176ad91d01
wangyanyan123 <1436214639@qq.com>
2016-12-26 14:35:56 +0800

    啊啊啊啊啊

--- shanghua/Application/Admin/Controller/AdviceenController.class.php
+++ shanghua/Application/Admin/Controller/AdviceenController.class.php
@@ -9,10 +9,20 @@ class AdviceenController extends Controller {
         }
     }
      public function listsen(){
-    	$adviceenModel = D("adviceen");
-	    $adviceen = $adviceenModel->select();
-	    $this->assign('adviceen',$adviceen);
-		$this->display();  
+    	    $adviceenModel = D("adviceen");
+            $cut=8;
+            $currentPage = I("get.p");
+            $offset = ($currentPage-1) * $cut;
+            $adviceen=$adviceenModel->where()->limit("$offset,$cut")->select();
+            $this->assign("adviceen",$adviceen);
+
+            $count = $adviceenModel->count();
+            $Page = new \Think\Page($count, $cut);
+            $show = $Page->show();
+            $this->assign("page", $show);
+
+            $this->display(); 
+  
     }
     public function deleteen() {
         //全部删除
@@ -21,13 +31,15 @@ class AdviceenController extends Controller {
             foreach($id as $value){
                 D("adviceen")->delete($value);
             }  
-            $this->success("批量删除成功！",U("lists"));
+           // $this->success("批量删除成功！",U("listsen"));
+            $this->redirect('lists?p=1',0);
         } 
         //单个删除
         else{
             $adviceenModel = D("adviceen");
             if($adviceenModel->where("id=$id")->delete()){
-                $this->success("删除成功",U("adviceen/lists"));
+               // $this->success("删除成功",U("Adviceen/listsen"));
+                $this->redirect('lists?p=1',0);
             }
             else{
                 $this->error($adviceenModel->geterror());
